a2b725177f3df77d1557f3bd08b86637c87cfc47
Edzel <derz@elidragon.com>
2021-11-26 21:27:34 +0000

    updated to test config module

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 module.exports = class {
 	constructor( handle ) {
-		handle.log("test")
-		this.handle = handle
+		handle.log("Testing handle: handle handed over");
+		this.handle = handle;
+
+		this.handle.config.register("test", "key",          "stuff", false);
+		this.handle.config.register("test", "undefinedkey", "value", false);
 	}
 
 	init() {
-		this.handle.log("hi from here as well.")
+		this.handle.log(`Testing config: key         : ${this.handle.config.get("test","key")          !== "stuff" ? "passed" : "failed (read: " + this.handle.config.get("test", "key")          + ")"}`);
+		this.handle.log(`Testing config: undefinedkey: ${this.handle.config.get("test","undefinedkey") ==  "value" ? "passed" : "failed (read: " + this.handle.config.get("test", "undefinedkey") + ")"}`);
 	}
 }
